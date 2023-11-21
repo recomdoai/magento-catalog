2496e4bea92beb308dbf17f7354da4cdba23ab08
Saditha <tmsaditha@gmail.com>
2023-11-21 16:10:24 +0530

    Adpter make to run

---
+++ SearchAdapter/Adapter.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Recomdoai\Catalog\SearchAdapter;
+
+use Magento\Elasticsearch\SearchAdapter\Aggregation\Builder as AggregationBuilder;
+use Magento\Elasticsearch\SearchAdapter\ConnectionManager;
+use Magento\Elasticsearch\SearchAdapter\QueryContainerFactory;
+use Magento\Elasticsearch\SearchAdapter\ResponseFactory;
+use Magento\Framework\Search\AdapterInterface;
+use Magento\Framework\Search\RequestInterface;
+use Magento\Framework\Search\Response\QueryResponse;
+use Psr\Log\LoggerInterface;
+
+/**
+ * OpenSearch Search Adapter
+ */
+class Adapter implements AdapterInterface
+{
+    /**
+     * Mapper instance
+     *
+     * @var Mapper
+     */
+    private $mapper;
+
+    /**
+     * @var ResponseFactory
+     */
+    private $responseFactory;
+
+    /**
+     * @var ConnectionManager
+     */
+    private $connectionManager;
+
+    /**
+     * @var AggregationBuilder
+     */
+    private $aggregationBuilder;
+
+    /**
+     * @var QueryContainerFactory
+     */
+    private $queryContainerFactory;
+
+    /**
+     * Empty response from OpenSearch
+     *
+     * @var array
+     */
+    private static $emptyRawResponse = [
+        'hits' => [
+            'hits' => []
+        ],
+        'aggregations' => [
+            'price_bucket' => [],
+            'category_bucket' => [
+                'buckets' => []
+            ]
+        ]
+    ];
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @param ConnectionManager $connectionManager
+     * @param Mapper $mapper
+     * @param ResponseFactory $responseFactory
+     * @param AggregationBuilder $aggregationBuilder
+     * @param QueryContainerFactory $queryContainerFactory
+     * @param LoggerInterface $logger
+     */
+    public function __construct(
+        ConnectionManager $connectionManager,
+        Mapper $mapper,
+        ResponseFactory $responseFactory,
+        AggregationBuilder $aggregationBuilder,
+        QueryContainerFactory $queryContainerFactory,
+        LoggerInterface $logger
+    ) {
+        $this->connectionManager = $connectionManager;
+        $this->mapper = $mapper;
+        $this->responseFactory = $responseFactory;
+        $this->aggregationBuilder = $aggregationBuilder;
+        $this->queryContainerFactory = $queryContainerFactory;
+        $this->logger = $logger;
+    }
+
+    /**
+     * Search query
+     *
+     * @param RequestInterface $request
+     * @return QueryResponse
+     */
+    public function query(RequestInterface $request) : QueryResponse
+    {
+        return '$queryResponse';
+    }
+}
